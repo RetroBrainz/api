da9bf3cc64119a5b6f1bdf6ae90939ca4e1b363a
Guo Yunhe <i@guoyunhe.me>
2025-03-01 14:46:19 +0800

    fix(game): date validation

--- app/validators/game.ts
+++ app/validators/game.ts
@@ -1,15 +1,16 @@
 import vine from '@vinejs/vine';
+import { DateTime } from 'luxon';
 
 export const createGameValidator = vine.compile(
   vine.object({
     name: vine.string().trim(),
-    releasedAt: vine.date(),
+    releasedAt: vine.date().transform((v) => DateTime.fromJSDate(v)),
   }),
 );
 
 export const updateGameValidator = vine.compile(
   vine.object({
     name: vine.string().trim(),
-    releasedAt: vine.date(),
+    releasedAt: vine.date().transform((v) => DateTime.fromJSDate(v)),
   }),
 );
