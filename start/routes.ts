25390afc61c55079e507139edd036ff5d7767db7
Guo Yunhe <i@guoyunhe.me>
2025-03-01 16:22:28 +0800

    feat(auth): routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,10 +9,17 @@
 
 import router from '@adonisjs/core/services/router';
 
+const AuthController = () => import('#controllers/auth');
+const GameController = () => import('#controllers/game');
+
 router.get('/', async () => {
   return {
     hello: 'world',
   };
 });
 
-router.resource('games', () => import('#controllers/game'));
+router.post('/login', [AuthController, 'login']);
+router.post('/logout', [AuthController, 'logout']);
+router.post('/register', [AuthController, 'register']);
+
+router.resource('games', GameController);
